3f805af9900419c610c53e89a7c7a47610e78ea2
pesquefGH <pesquef@lncc.br>
2026-06-30 11:12:35 -0300

    Add files via upload

--- LNCC_ARp26_dengue/validation3/matlab/v3_LNCC_ARp26_forecast.m
+++ LNCC_ARp26_dengue/validation3/matlab/v3_LNCC_ARp26_forecast.m
@@ -185,7 +185,9 @@ indf_end=716+52+52; % time index of the EW 40 2025
 
 % Writes a CSV file with the known and forecast data
 
-date=S_dates(indf_ini:indf_end);  % epidemic weeks to be forecast 
+date=S_dates(771:822);  % Sunday days for validation 3
+
+
 pred_range=indf_end+1-indf_ini; % forecast range
 gapf=15;  % gap in samples from EW 26 2024 to EW 40 2024  
 pred(1:gapf)=[]; pred=pred(1:pred_range);
