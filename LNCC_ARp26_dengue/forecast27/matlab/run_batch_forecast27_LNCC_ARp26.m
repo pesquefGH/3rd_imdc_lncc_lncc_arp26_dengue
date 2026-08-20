674a00d8ee45583b15304a310d66e04e6805c369
pesquefGH <pesquef@lncc.br>
2026-08-20 12:57:56 -0300

    Add files via upload

---
+++ LNCC_ARp26_dengue/forecast27/matlab/run_batch_forecast27_LNCC_ARp26.m
@@ -0,0 +1,14 @@
+% Runs Forecast 27 with LNCC ARp26 dengue forecast model for all Brazilian states
+clear all; close all; clc
+
+addpath ..\..\..\Aggregated_Data
+
+UFv={'AC','AL','AM','AP','BA','CE','DF','ES','GO','MA','MG','MS',...
+    'MT','PA','PB','PE','PI','PR','RJ',...
+    'RN','RO','RR','RS','SC','SE','SP','TO'};
+
+for n=1:length(UFv)
+    UF=UFv{n};
+    forecast_27_LNCC_ARp26;
+end
+
